d91c5c72bb91a5c9861ccd6369f74323bb3b071e
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-10 23:52:16 +0100

    Demande d'informations aux utilisateurs

---
+++ exemple.js
@@ -0,0 +1,24 @@
+/*Afficher une information*/
+
+// alert('Hello World');
+
+
+/*Demander une confirmation à l'utilisateur*/
+
+// if (confirm('Souhaitez-vous confirmer?'))       // Le fait de mettre if suivi du 'confirm', implique d'entrée de jeu que l'on a appuyé sur oui.
+// {
+//     alert('Oui');                               // Affiche oui si on clique sur ok
+// }
+// else
+// {
+//     alert('Annulation');                        // Affiche annulation si on clique sur Annuler
+// }
+
+
+/*Demander des renseignements à l'utilisateur */
+
+let ageInfo = prompt("Entrez votre âge: ");         // On peut stocker la valeur donnée par l'utilisateur dans une variable qui contiendrait le 'prompt'.
+
+
+
+alert("Bonjour, vous avez " + ageInfo + " ans!");
