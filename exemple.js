e2de9ba73ce0d0817d847c71cf7811a7004bf3fc
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-12 00:00:33 +0100

    Fonctions anonymes

--- exemple.js
+++ exemple.js
@@ -1,22 +1,23 @@
-/* Conversion de données */
+/* Fonctions anonymes */
 
-function prévoirAge() {
+// function(){
+//     console.log('Je suis une fonction anonyme.');
+// }
 
-    let age = prompt("Quel age avez vous?");            // Javascript stock les informations recueillies chez les utilisateurs sous forme de string.
-    // age = parseInt(age);                                // La var. age a été transformée en entier le plus proche, de sorte à ce que l'addition se déroule bien dans le alert(); 
-    // age = parseFloat(age);                              // Cette méthode permet de conserver le nombre tel quel, même avec virgule.
-    age = Number(age);                                     // La solution number(); est la plus sûre.
-    alert("Bientôt vous aurez " + (age + 1) + " ans.");
+/* Usage dans une variable */
+
+let fonctionAnonyme = function(){
+
+    console.log('Je suis une fonction anonyme.');
 }
 
-prévoirAge();
+
+fonctionAnonyme();
+
+/* Auto-execution */
+
+(function(){ console.log("Je suis une fonction anonyme qui s'auto-execute.") })();
 
 
 
-/* Convertir un nombre en string
 
-nb = 45;
-let nbEnString = nb + "";               // Cette 1ère méthode permet de convertir le nombre en chaîne de caractères.
-let nbEnString = nb.toString();         // La 2e méthode consiste à utiliser la fct. toString().
-
-*/
