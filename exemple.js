cd7463f2be5514ae30ea164fc7262333f75f0204
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-11 15:49:18 +0100

    Variables locales et globales

--- exemple.js
+++ exemple.js
@@ -1,26 +1,20 @@
-// /* Fonction sans paramètres */
+/* Variables locales et globales*/
 
-// function direBonjour() {
-//     alert('Bonjour Ahmed');         // alert(); est une fct. déjà intégrée à javascript. On lui a donné un param.
-// }
+let cri = "Toujours plus bas!";      // Globale
 
-// direBonjour();
+function criDeGuerre() {
 
-
-/* Fonctions avec paramètres */
-
-function addition(nbUn, nbDeux) {       // Les param. sont des variables automatiquement déclarées et modifiables.
-    
-    console.log(nbUn + nbDeux)
+    let cri = "Toujours plus fort!"; // Locale
+    console.log(cri); 
 
 }
 
-addition(5, 9.5);                       // On doit obligatoirement donner des param. à l'appel d'une fct. qu'on a déclarée avec des param.
+criDeGuerre();                      // La fct. n'affichera en priorité sa variable locale.
+
+console.log(cri);                   // La console affichera la var. globale.
 
 
-function direBonjour(prenom) {
-    alert('Bonjour ' + prenom);
-}
 
-direBonjour('Ahmed');
+
+
 
