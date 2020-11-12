3ac1fbd0f3b7e31db3444f99d40361357fe7924c
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-12 13:18:58 +0100

    Exercices sur les fonctions

--- exemple.js
+++ exemple.js
@@ -1,23 +1,38 @@
-/* Fonctions anonymes */
+// /* Exercice débutant */
 
-// function(){
-//     console.log('Je suis une fonction anonyme.');
+// function abracadabra(){
+
+//     let prenom = prompt("Quel est votre prénom?"),
+//     nom        = prompt("Quel est votre nom?"),
+//     age        = prompt("Quel age avez-vous?");
+
+//     alert("Sapristi! On ne m'avait pas prévenu que c'était vous, " 
+//     + prenom 
+//     +"! Euh je veux dire... Monsieur le grand magicien " 
+//     + nom 
+//     + "! Cela fait déjà " 
+//     + age 
+//     + " ans que vous faites rayonner notre contrée!");
 // }
 
-/* Usage dans une variable */
+// abracadabra();
 
-let fonctionAnonyme = function(){
 
-    console.log('Je suis une fonction anonyme.');
+
+/* Exercice avancé */
+
+
+
+function calculerIMC(taille, poids) {
+
+    let imc = poids / Math.pow((taille/100), 2);
+    return imc;
 }
 
+let poids = prompt("Quel est votre poids?");
+let taille = prompt("Quelle taille faites-vous? (en cm)");
 
-fonctionAnonyme();
+let votreIMC = calculerIMC(taille, poids);
 
-/* Auto-execution */
-
-(function(){ console.log("Je suis une fonction anonyme qui s'auto-execute.") })();
-
-
-
+alert("Votre IMC est de: " + votreIMC + "!");
 
