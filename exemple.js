d29b3b30062f70bf622ee8ce8208757341ab2310
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-13 00:04:16 +0100

    Projet calculatrice

--- exemple.js
+++ exemple.js
@@ -1,41 +1,78 @@
 
-try {
-
-    let ustencile = prompt("Choisissez votre instrument: couteau, cuillère, fourchette.");
-    let utilité;
-    switch (ustencile){
-        case "couteau":
-            utilité = 40;                       // Le try s'exécute, et en cas d'erreur, c'est le catch qui s'exécute.
-            break;                              // On crée et définit une situation d'erreur qui fera s'exécuter le catch.
-        case "cuillère":                        // Ici on a utilisé le switch et définit des valeur pour lesquelles il ya erreur.
-            utilité = 50;                       // On utilise le 'throw new Error' pour renvoyer directement vers le catch.
-            break;                              // Dans le catch, le alert fera en sorte de mettre le contenu de l'Error dans une boîte de dialogue.
-        case "fourchette":                      // On aurait pu aussi le mettre dans la console grâce à: console.log(erreur)
-            utilité = 30;
-            break;
-
-        default: 
-            throw new Error("Vous ne pouvez pas choisir cet instrument.");
+    function addition(nombreA, nombreB){
+        return nombreA + nombreB;
     }
 
-    alert("Vous avez choisi l'ustencile " + ustencile + " (utilité: " + utilité + ").");
+    function multiplication(nombreA, nombreB){
+        return nombreA * nombreB;
+    }
 
-} 
+    function soustraction(nombreA, nombreB){
+        return nombreA - nombreB;
+    }
 
-catch (erreur) {
+    function division(nombreA, nombreB){
 
-    alert(erreur.stack);
-    
+        if(nombreB == 0){
+            throw new Error("Impossible de diviser par 0");        
+        }
+
+        return nombreA / nombreB;
+    }
+
+let restart = false;
+
+do{
+
+    do {
+        
+        var choix = Number(prompt("Que souhaitez-vous faire?\n\n 1.Addition \n 2.Multiplication \n 3.Soustraction \n 4.Division"));
+
+
+
+    } while (choix != 1 && choix != 2 && choix != 3 && choix != 4);
+
+
+    do {
+        
+        var premierNombre = Number(prompt("Entrez le premier nombre."));
+        var deuxiemeNombre = Number(prompt("Entrez le deuxieme nombre."));
+
+
+    } while (isNaN(premierNombre) || isNaN(deuxiemeNombre) );
+
+
+    var resultat;
+
+    try {
+        switch (choix) {
+            case 1:
+                resultat = addition(premierNombre, deuxiemeNombre);
+                break;
+            case 2:
+                resultat = multiplication(premierNombre, deuxiemeNombre);
+                break;
+            case 3:
+                resultat = soustraction(premierNombre, deuxiemeNombre);
+                break;
+            case 4:
+                resultat = division(premierNombre, deuxiemeNombre);
+                break;
+                            
+            default:
+                throw new Error ("Une erreur est survenue.");
+        }
+
+        alert("Le resultat est de: " + resultat)
+
+
+    } catch (error) {
+                alert(error);
+    }
+
+    restart = confirm("Souhaitez-vous faire un nouveau calcul?");
 }
-
-finally {
-    alert("Fin du programme.");         // Le finally, sert à faire s'exécuter un code à coups sûr.
-}
-
-
-
-
-
+while(restart)
 
 
 
