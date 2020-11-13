43dfd47419302b81330083ed1b4a9961ca4a4d3a
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-13 17:08:01 +0100

    Concaténer un tableau associatif

--- exemple.js
+++ exemple.js
@@ -1,18 +1,39 @@
-/*Boucles pour tableaux*/
+/* Concaténer un tableau associatif */
 
-let listeDePays = ['Algerie', 'Belgique', 'Japon', 'Maroc'];
 
-// for (const pays of listeDePays) {
-//     console.log(pays);
+let monTableauAssociatif = {
+    'prenom' : 'Mark',
+    'nom' : 'Zuckerberg',
+    'poste' : 'PDG de Facebook'
+}
+
+// let chaine = '';
+
+// for (const valeur in monTableauAssociatif) {
+
+//     chaine += (valeur + ' : ' + monTableauAssociatif[valeur] + '\n');
+    
 // }
+// console.log(chaine);
 
 
-// listeDePays.forEach(function(pays){      // On utilise ici une fonction anonyme.
-//     console.log(pays);
-// });
+function concatener(tableau) {
+
+    let chaine = '';
+
+    for (const valeur in tableau) {
+
+        chaine += (valeur + ' : ' + tableau[valeur] + '\n');
+    
+    }
+
+    console.log(chaine);
+
+    
+}
 
 
-listeDePays.forEach(pays => console.log(pays));   // On efface la fct. et on utilise une fonction fléchée. Nouveauté javascript.
+concatener(monTableauAssociatif);
 
 
 
