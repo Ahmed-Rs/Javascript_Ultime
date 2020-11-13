7e78434808ee728b7efb160fc5dc0d98eb13b529
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-14 00:09:13 +0100

    Les fermetures / Closures

--- exemple.js
+++ exemple.js
@@ -1,28 +1,59 @@
-/* Fonctions fléchées */
-
-// let maFonction = function () {
-//     console.log('test');    
-// }
+/* Les fermetures/ Closures */
 
 
-// let maFonction = () => {            //On met les param. dans les parenthèses s'il y'en a plus d'un, mais il n'y en a qu'un seul, on ne met pas de prenthèses.
-//     console.log('test');
-// }
+function bonjour(prenom) {
 
-let maFonction = () => console.log('test'); // Condensé de la fct. anonyme en une ligne
+    let resultat = "Bonjour " + prenom; // Variable locale
+    let maClosure = () => console.log(resultat); // La closure utilise la var. loc. resultat et la sauvegarde ainsi.
 
+    return maClosure;   // Il faut que la fonction retourne la closure, afin que les var. locales soient sauvegardées et définies à l'extérieur de la fonction .
+}
+
+
+
+let maFonction = bonjour('Evan');   // Ici on stock la var. resultat, qui est locale. Ce n'est possible que grâce à la closure.
+maFonction();
 maFonction();
 
 
+// A la fin de la fonction 'bonjour', les var. resultat et prenom sont normalement effacées, mais la closure les sauvegarde, avec leurs valeurs.
 
 
 
+function bonjour_bis(prenom) {
+
+    let resultat = "Bonjour " + prenom;
+    console.log(resultat);
+}
+
+bonjour_bis('Nicolas'); // A la différence des closures, ici on doit tout le temps écrire la variable Nicolas pour que la fct. l'affiche.
+                        // Or qu'avec les closures, la variables est sauvegardée et l'appel simple de la fct. suffit à l'afficher.
+bonjour_bis('Nicolas');
 
 
 
+function timer() {
+    let secondes = 0;
+    let maFctClosure = () => {
+        return ++secondes;
+    } 
+
+    return maFctClosure;
+
+}
+
+let monTimer = timer();
+console.log(monTimer());
+console.log(monTimer());
+console.log(monTimer());
+console.log(monTimer());        // On lit bien sur la console que la valeur de secondes est incrémentée, ce qui montre que la closure a sauvegardé la valeur de la var. locale de la fct.
+
+let monDeuxiemeTimer = timer(); // En créant une deuxieme var, On réinitialise la fct. 
+console.log(monDeuxiemeTimer());
+console.log(monDeuxiemeTimer());
 
 
-
+console.log(monTimer());   // La première variable reste toujours sauvegardée pour lorsque la fct. monTimer(); sera appelée.
 
 
 
