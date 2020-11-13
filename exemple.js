908806f6926f3ac31edb95aa230bf5db908a7067
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-13 11:59:46 +0100

    Récursivité

--- exemple.js
+++ exemple.js
@@ -1,81 +1,32 @@
+/* Récursivité */
 
-    function addition(nombreA, nombreB){
-        return nombreA + nombreB;
-    }
 
-    function multiplication(nombreA, nombreB){
-        return nombreA * nombreB;
-    }
+// function timer(secondes) {
 
-    function soustraction(nombreA, nombreB){
-        return nombreA - nombreB;
-    }
+//     if (secondes > 0) {                     // Une fct. qui s'appelle elle-même.
 
-    function division(nombreA, nombreB){
-
-        if(nombreB == 0){
-            throw new Error("Impossible de diviser par 0");        
-        }
-
-        return nombreA / nombreB;
-    }
-
-let restart = false;
-
-do{
-
-    do {
+//         console.log(secondes);
+//         timer(secondes - 1);
         
-        var choix = Number(prompt("Que souhaitez-vous faire?\n\n 1.Addition \n 2.Multiplication \n 3.Soustraction \n 4.Division"));
+//     } else {
+//         console.log(secondes);
+//     }
+    
+// }
+
+// timer(10);
 
 
+function somme(nombre) {
 
-    } while (choix != 1 && choix != 2 && choix != 3 && choix != 4);
-
-
-    do {
-        
-        var premierNombre = Number(prompt("Entrez le premier nombre."));
-        var deuxiemeNombre = Number(prompt("Entrez le deuxieme nombre."));
-
-
-    } while (isNaN(premierNombre) || isNaN(deuxiemeNombre) );
-
-
-    var resultat;
-
-    try {
-        switch (choix) {
-            case 1:
-                resultat = addition(premierNombre, deuxiemeNombre);
-                break;
-            case 2:
-                resultat = multiplication(premierNombre, deuxiemeNombre);
-                break;
-            case 3:
-                resultat = soustraction(premierNombre, deuxiemeNombre);
-                break;
-            case 4:
-                resultat = division(premierNombre, deuxiemeNombre);
-                break;
-                            
-            default:
-                throw new Error ("Une erreur est survenue.");
-        }
-
-        alert("Le resultat est de: " + resultat)
-
-
-    } catch (error) {
-                alert(error);
+    if (nombre == 1) {              // Valeur 1 d'arrivée
+        return 1;
     }
 
-    restart = confirm("Souhaitez-vous faire un nouveau calcul?");
+    return nombre + somme(nombre - 1); // La fonction s'execute en boucle: Elle dépose un 'nombre' et ensuite va se réexécuter jusqu'à ce que nombre == 1.
 }
-while(restart)
-
-
-
+ 
+console.log(somme(5));
 
 
 
