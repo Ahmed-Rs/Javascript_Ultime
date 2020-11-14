3aedc30546004536e19206f23bfe3188feb5ede4
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-14 21:41:22 +0100

    Objet Set

--- exemple.js
+++ exemple.js
@@ -1,21 +1,33 @@
-/* L'affectation par décomposition / Destructuring */
+/* L'objet Set */
+
+// Les set ne renvoient pas de valeurs doubles, et on peut y stocker tous types de données; entiers, strings, arrays...
 
 
-let informations = ['SuperSayen', '25', 'homme'];
+let nombres = [10, 39, 69, 39, 19, 10];
 
-// Sans décomposition
-// let pseudo = informations[0];
-// let age    = informations[1];   // En mettant les informations dans des variables, si on modifie celles-ci, celà ne modifiera pas les éléments du tab.
-// let sexe   = informations[2];   // Contrairement à si on modifiait directement information[x].
+// let monSet = new Set(nombres);
+let monSet = new Set(nombres);
 
 
-// Avec décomposition
 
-let [pseudo, age, sexe] = informations; // On met toutes ces variables dans une seule variable 'tableau' liée respectivement au tableau 'informations'
-                                        // La console ira chercher la valeur en question dans le tableau.
-console.log(pseudo);
-console.log(age);
-console.log(sexe);
+monSet.add(47);
+monSet.delete(69);
+
+console.log(monSet);
+console.log('La taille de mon Set : ' + monSet.size);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
