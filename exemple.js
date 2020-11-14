f64b9eb3ef2e536bcd32dd4100c71f854ce2c975
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-14 16:03:42 +0100

    L'affectation par décomposition / Destructuring

--- exemple.js
+++ exemple.js
@@ -1,13 +1,22 @@
-/* Les Objets */
+/* L'affectation par décomposition / Destructuring */
 
-let chien = {                   // L'objet chien a des propriétés au lieu de variables, et des méthodes au lieu de fonctions.
-    race: 'chiba',
-    poil: 'court',
-    aboyer: () => console.log('wafwaf')
 
-}
+let informations = ['SuperSayen', '25', 'homme'];
 
-chien.aboyer();
+// Sans décomposition
+// let pseudo = informations[0];
+// let age    = informations[1];   // En mettant les informations dans des variables, si on modifie celles-ci, celà ne modifiera pas les éléments du tab.
+// let sexe   = informations[2];   // Contrairement à si on modifiait directement information[x].
+
+
+// Avec décomposition
+
+let [pseudo, age, sexe] = informations; // On met toutes ces variables dans une seule variable 'tableau' liée respectivement au tableau 'informations'
+                                        // La console ira chercher la valeur en question dans le tableau.
+console.log(pseudo);
+console.log(age);
+console.log(sexe);
+
 
 
 
