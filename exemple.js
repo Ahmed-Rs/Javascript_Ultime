c98b52e9792ac9ef48ec5b4cb7574db2656046f5
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-14 22:39:24 +0100

    Ajout des WeakSet

--- exemple.js
+++ exemple.js
@@ -1,44 +1,32 @@
-/* L'objet Map */
+/* L'objet WeakSet */
+
+// Le WeakSet, n'accèpte que les Objets (propriété, valeur)
 
 
-// let monMap = new Map([
-//     ['prenom' , 'John'],
-//     ['nom'    , 'Neeson']
-// ]);
-
-// monMap.set('poste', 'Acteur');      //  Ajouter une clé , valeur
-// monMap.delete('nom');               // Supprimer une clé, valeur
-
-// console.log(monMap);
+// let voitures = new WeakSet();
 
 
-
-let utilisateurs = new Map();
-
-
-utilisateurs.set('Mark Zukerberg', {        // Ici on entre une clé, valeurs, avec plusieurs valeurs, comme un tableau associatif.
-    email: 'mark@gmail',
-    poste: 'PDG',
-});
+let voitureA = {
+    marque: 'Mercedes',
+    modele: 'SLK Gtr'
+}
 
 
-utilisateurs.set('Bill Gates', {           // On recommence l'opération pour entrer un nouvel utilisateur.
-    email: 'bill.com',
-    poste: 'PDG',
-});
-
-console.log(utilisateurs);
+let voitureB = {
+    marque: 'BMW',
+    modele: 'Z3'
+}
 
 
+// voitures.add(voitureA); // On ajoute un objet
+// voitures.add(voitureB);
+// voitures.delete(voitureA); // On retire un objet
+// Pour ne pas avoir à écrire plusieur 'add()', on peut déclarer le weakSet après les objets qu'il contiendra, afin de les lui faire passer en arguments.
 
 
+let voitures = new WeakSet([voitureA, voitureB]);
 
 
-
-
-
-
-
-
+console.log(voitures);
 
 
