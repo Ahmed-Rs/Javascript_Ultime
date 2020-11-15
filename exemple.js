cac3a3d3de11d69768944041713d895c7211c17c
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-15 15:20:06 +0100

    Objet Navigator

--- exemple.js
+++ exemple.js
@@ -1,38 +1,17 @@
 /* BOM */
 
-// L'objet principal: Window
-
-// Ouvrir une fenêtre
-window.open('https://believemy.com');
-
-
-// Redimensionner une fenêtre
-
-let fenetre = 
-window.open('https://believemy.com', '', 'width=900', 'height=700');
-
-
-function resize() {
-    fenetre.resizeTo(700, 470);
-}
-
-resize();
-
-
-// Fermer une fenêtre
-
-// function resize(){
-//     fenetre.close();
-// }
-
-// close();
+// L'objet Navigator
 
 
 
+// Les cookies sont-ils autorisés? true
+navigator.cookieEnabled
 
+// Système d'exploitation? Win32
+navigator.platform
 
-
-
+//Langue du navigateur? fr-FR
+navigator.language
 
 
 
