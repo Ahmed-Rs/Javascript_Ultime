e906f154cebf5f3bf640e2944c7b225a70e48f2c
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-15 15:44:20 +0100

    Objets Hitsory, Location, Screen, Document

--- exemple.js
+++ exemple.js
@@ -1,19 +1,51 @@
 /* BOM */
 
-// L'objet Navigator
+/* L'objet HISTORY */
+
+
+// Page précédente
+history.back();
+
+// Page suivante
+history.forward();
 
 
 
-// Les cookies sont-ils autorisés? true
-navigator.cookieEnabled
 
-// Système d'exploitation? Win32
-navigator.platform
+/* L'objet LOCATION */
 
-//Langue du navigateur? fr-FR
-navigator.language
+// L'objet Location a pour fonction de nous donner des informations relatives aux adresses, pour, par exemple, rediriger l'utilisateur.
+// Voici une petite démonstration de quelques fonctions bien utiles que propose cet objet.
+
+// Recharger une page
+location.reload();
+
+// Rediriger
+location.href();
 
 
+/* L'objet SCREEN */
+
+// L'objet Screen a pour fonction de nous donner des informations relatives aux écrans.
+// Voici une petite démonstration de quelques fonctions bien utiles que propose cet objet.
+
+// Largeur de l'écran
+screen.availWidth;
+
+// Hauteur de l'écran
+screen.availHeight;
+
+// Résolution de l'écran
+screen.pixelDepth;
+
+// Syntaxe
+
+objetEcran = window.screen;
+
+/* L'objet DOCUMENT */
+
+// C'est l'objet qui permet de modifier le HTML de notre site
+// Lorsque l'utilisateur va cliquer sur un boutton, appuyer sur touche, envoyer un formulaire, etc...
 
 
 
