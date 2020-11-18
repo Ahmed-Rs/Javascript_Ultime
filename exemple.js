2b517353972bd39225f37f8d8ae5d3205b0a3b41
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-18 23:40:11 +0100

    Créer un objet avec le constructeur Object

--- exemple.js
+++ exemple.js
@@ -20,42 +20,34 @@ function Utilisateur(prenom, nom, email) {      // On initilialise une fct. cons
     this.prenom = prenom;
     this.nom    = nom;
     this.email  = email;
+}
 
-    this.sePresenter = () => {                 // On ajoute une fct. anonyme dans une nouvelle propriété.
-        console.log("Bonjour je m'appelle " + this.prenom + " " + this.nom + " et vous pouvez me joindre à l'adresse: "+ this.email + ".");
-    }
+// On retire la méthode 'sePresenter' de notre constructeur pour la mettre dans le prototype de ce dernier.
+
+Utilisateur.prototype.sePresenter = function() {                 // On ajoute une fct. anonyme dans une nouvelle propriété.
+    console.log("Bonjour je m'appelle " + this.prenom + " " + this.nom + " et vous pouvez me joindre à l'adresse: "+ this.email + ".");
 }
 
 
+// On crée un Objet
 var mark = new Utilisateur("Mark", "Zuckerberg", "mark.com");
 var bill = new Utilisateur("Bill", "Gates", "bill.com");
-// mark.sePresenter();        // Ne pas oublier d'initialiser la fct. à l'intérieur de l'objet.
 
-mark.poste = "PDG de Facebook";  // On peut rajouter des éléments personnalisés à notre Objet mark.
+console.log(mark);  // On voit que JavaScript ajoute une propriété __proto__ et constructor
 
-// console.log(mark);
-
-// console.log(bill);
-
-// Utilisons un Objet comme paramètre dans un autre Objet
-
-function Logement(type, annee, placeDeParking, propriétaire) {
-    this.type           = type;
-    this.annee          = annee;
-    this.placeDeParking = placeDeParking;
-    this.propriétaire   = propriétaire;
-}
-
-var appartementZ = new Logement("appartement", "2020", true, mark); // mark est un Objet
-
-console.log(appartementZ.propriétaire.nom);     // On appelle la propriété nom, dans l'Objet Utilisateur(mark) contenu dans l'Objet Logement(appartementZ)
-
-console.log(appartementZ);
+mark.sePresenter();  // On rajoute la fct. sePresenter dans le prototype du constructeur Utilisateur, ainsi ils pourront aussi en hériter.
+                     // Bill aussi peut se présenter
 
 
+/*Créer un objet avec Object(); */
 
+var monObjet = new Object();
+monObjet.titre = "Le titre de l'objet";
 
+// console.log(monObjet);
 
+// Lorsque le navigateur va lire la fct. sePresenter(); il la cherchera d'abord dans le constructeur, ensuite dans la propriété __proto__ de l'Objet que nous avons créé(mark), puis dans la __proto__ (prototype) du constructeur Objet() (JS) qui a créé notre constructeur,
+// jusqu'à tomber sur la propriété constructor de notre Object() JS; S'il ne trouve pas la fct. sePresenter, il va afficher un message d'erreur. Celà s'appelle remonter ma chaîne des prototypes.
 
 
 
