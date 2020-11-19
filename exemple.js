22aeedf0764b6300720d598b86a8a82e30b9d2cb
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-19 23:21:26 +0100

    Créer un objet avec une class et un constructeur.

--- exemple.js
+++ exemple.js
@@ -1,63 +1,57 @@
 /* Programmation Orientée Objet */
 
-// Bind, Call, Apply
+// Les class avec JavaScript
 
-// Call et Apply permettent d'invoquer immédiatement une fonction
+class Utilisateur {                         // On déclare une class dans laquelle on intègre un constructor et une méthode. On n'a pas besoin de passer la méthode en prototype comme avec les Objets de constructor.
+    constructor(prenom, nom, email) {       // 
+        this.prenom = prenom;
+        this.nom    = nom;
+        this.email  = email;
+    }
 
-
-let gandalf = { titre: "Magicien"}      // Objet littéral
-
-// La fct direBonjour() va utiliser this.titre grâce à l'appel de l'objet gandalf.
-
-function direBonjour(arme, degats) {
-    console.log("Bonjour " + this.titre + ", vous possédez " + arme + " qui donne " + degats + " points de dégats.");
-}
-
-// Avec call, on appelle l'objet gandalf et les autres arguments un à un.
-direBonjour.call(gandalf, "un bâton", 75);
-
-// Avec apply, on appelle aussi l'objet gandalf, mais les autres arguments seront appelés comme un tableau.
-direBonjour.apply(gandalf, ["un bâton", 75]);
-
-
-//Bind
-// Objectif
-
-this.valeur = "Window"; // Pour tous les navigateurs, this. écrit en dehors d'une fonction ou d'un objet vaut automatiquement "window" */
-
-
-let monObjet = {
-    valeur: "Objet",
-    getValeur: function () {
-        console.log(this.valeur);
+    sePresenter() {
+        console.log("Bonjour, je m'appelle " + this.prenom + ' ' + this.nom + " et vous pouvez me contacter à l'adresse: " + this.email);
     }
 }
 
-monObjet.getValeur();   // Ici, le navigateur nous affiche automatiquement la valeur de this.valeur dans le contexte de l'Objet littéral monObjet();
+var mark = new Utilisateur("Mark", "Zuckerberg", "marc@facebook.com");
+mark.sePresenter();
+
+// Animal
+
+class Animal {
+    constructor(nombreDePattes, poids) {
+        this.nombreDePattes = nombreDePattes;
+        this.poids          = poids;
+    }
+
+    description () {
+        console.log("Bonjour, cet animal possède " + this.nombreDePattes + " pattes et pèse " + this.poids + " Kg.");
+    }
+}
 
 
-let maValeur = monObjet.getValeur; // On stock la fct. getValeur() dans une variable
-maValeur(); // En exécutant cette variable,le nav. nous renvoie "window", car on est dans un contexte hors de la fct. monObjet(). Le this.valeur est celui du contexte global.
+// var panthere = new Animal(4, "100");
+// panthere.description();
 
-let maValeurAvecBind = monObjet.getValeur.bind(monObjet);   // Avec bind(), le contexte de 'monObjet.getValeur' est le contexte de l'objet monObjet();
-maValeurAvecBind();                                         // Avec bind, on précise le contexte.
+/* L'héritage avec les class */
 
+class Oiseau extends Animal {                   // Avec extends on fait hériter la class Oiseau de la class Animal 
+    constructor(nombreDePattes, poids, longueurDesAiles) {   // On rajoute les arg. du constructeur de Animal au constructeur de Oiseau.
+        super(nombreDePattes, poids);                        // La fct. super sert à appeler automatiquement la class Animal, de la même façon que le Call
+        this.longueurDesAiles = longueurDesAiles;
+    }
 
+    voler() {
+        console.log("L'oiseau vole avec des ailes d'une envergure de " + this.longueurDesAiles + ".");
+    }
+}
 
+var perroquet = new Oiseau(2, "1", "40 cm");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(perroquet);
+perroquet.description();        // Grâce à l'héritage, on peut appeler la fct.description de la class Animal.
+perroquet.voler();
 
 
 
