ff24b587300d4a68b5b00871b5bbb876b2165260
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-21 23:18:28 +0100

    Transformer une date au format local

--- exemple.js
+++ exemple.js
@@ -1,54 +1,28 @@
 /* Devenez maîtres du temps */
 
-// Récupérer la date actuelle
-
-// 1er janvier 1970 à minuit UTC (Londres)
-
-let dateActuelle = Date();
-console.log(dateActuelle);  // norme ango-saxonne
-
-let dateEnSecondes = Date.now();
-console.log(dateEnSecondes);
+// Mettre une date au format local
 
 
-// Utiliser l'Objet Date
+let dateActuelle = new Date();
 
-let datePrecise = new Date(2020, 11, 3, 2);
-console.log(datePrecise);
+// toLocaleDateString = jour, mois, année
+// toLocaleTimeString = l'heure
+// toLocaleString     = jour, mois, année, heure
+// ON CHOISIT LES OPTINONS QU'ON VEUT
 
+let dateLocale = dateActuelle.toLocaleString('fr-FR', {
+    weekday: 'long', // short(3 premières lettres), narrow(première lettre), long(ttles lettres) (nom du jour)
+    year: 'numeric', // numeric(xxxx), 2-digit(xx)
+    month: 'long',   // short, narrow, long, numeric
+    day: 'numeric',  // numeric, 2-digit
+    // hour: 'numeric', // ...
+    // minute: 'numeric',
+    // second: 'numeric'
+});
 
-
-// Au-dessus :
-//   - annee (obligatoire)
-//   - mois (obligatoire) qui commence par 0
-
-//   - jour (facultatif) qui commence par 1
-//   - heure (facultatif) qui commence par 0
-//   - minute (facultatif) qui commence par 0
-//   - secondes (facultatif) qui commence par 0
-//   - millisecondes (facultatif) qui commence par 0
-
-
-/* Getter et Setter disponibles pour l'objet Date */
-
-let dateEnCours = new Date();
-
-console.log(dateEnCours.getFullYear()); // Récupère l'année en cours
-console.log(dateEnCours.getDay());      // Récupère le numéro de jour de la semaine en cours, en commencant à compter à partir de dimanche à l'indexe 0.
-console.log(dateEnCours.getDate());     // Récupère le numéro de jour dans le mois en commençant par 1.
-
-console.log(dateEnCours.getUTCDate);    // Récupère le jour UTC du même moment.
-
-
-
-
-dateEnCours.setFullYear(2030);          // Les Setter vont nous permettre de modifier les éléments souhaités dans la date en les écrivant simplement de cette manière.
-console.log(dateEnCours.getFullYear);
-
-
-
-
-
+console.log(dateLocale);
+console.log(navigator.language);    // Cela nous renvoie la langue locale de l'utilisateur
+// En mettant navigator.language à la place de fr-FR, on aurait eu le même résultat.
 
 
 
