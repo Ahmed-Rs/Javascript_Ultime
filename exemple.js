74404205cc6f7e2cccb67199b9eaad14869a1819
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-21 23:44:54 +0100

    Templates string avec les dates

--- exemple.js
+++ exemple.js
@@ -1,40 +1,32 @@
 /* Devenez maîtres du temps */
 
-// Mettre une date au format local
+// Utiliser les templates string avec les dates
 
 
-let dateActuelle = new Date();
-
-// toLocaleDateString = jour, mois, année
-// toLocaleTimeString = l'heure
-// toLocaleString     = jour, mois, année, heure
-// ON CHOISIT LES OPTINONS QU'ON VEUT
-
-let dateLocale = dateActuelle.toLocaleString('fr-FR', {
-    weekday: 'long', // short(3 premières lettres), narrow(première lettre), long(ttles lettres) (nom du jour)
-    year: 'numeric', // numeric(xxxx), 2-digit(xx)
-    month: 'long',   // short, narrow, long, numeric
-    day: 'numeric',  // numeric, 2-digit
-    // hour: 'numeric', // ...
-    // minute: 'numeric',
-    // second: 'numeric'
-});
-
-console.log(dateLocale);
-console.log(navigator.language);    // Cela nous renvoie la langue locale de l'utilisateur
-// En mettant navigator.language à la place de fr-FR, on aurait eu le même résultat.
+// // Avant
+// let prenom  = "John";
+// let bonjour = "Bonjour " + prenom;
+// console.log(bonjour);
 
 
+// // Maintenant
+// let prenom  = "John";
+// let bonjour = `Bonjour ${prenom}`;
+// console.log(bonjour);
 
 
+// // Avec les dates
+// let date      = new Date().getFullYear();
+// let copyright = `${date} (c) Believemy`;
+// console.log(copyright);
 
+ 
+// Ou encore plus pratique: des calculs
 
-
-
-
-
-
-
+let aliments = {fruits: 5, legumes: 1, biscuits: 56}
+let panier   = `Dans votre panier, vous avez ${aliments.fruits + 
+aliments.legumes + aliments.biscuits} articles.`;
+console.log(panier);
 
 
 
