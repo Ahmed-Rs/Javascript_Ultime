a4ed55b241a4c0b83df80dcde12f63a706eda4d0
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-23 23:14:41 +0100

    API Envoyer des données

--- exemple.js
+++ exemple.js
@@ -1,50 +1,36 @@
-/* Une application météo */
+/* Utilier les API */
+
+/* Utiliser une API: envoyer des données */
+
+const url = "https://lesoublisdelinfo.com/api.php"
+
+let requete = new XMLHttpRequest();
+
+// GET
+// requete.open('GET', url);
+// requete.responseType = "json";
+// requete.send();
 
 
+// POST
+requete.open("POST", url);
+requete.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+requete.responseType = 'json';
+requete.send('prenom=John');
 
-window.onload = function() {
-
-    let ville0 = "Paris";
-    recevoirTemperature(ville0);
-
-    let boutton = document.querySelector("#changer");
-    boutton.addEventListener('click', () => {
-        let villeChoisie = prompt("Veuillez taper le nom d'une ville.");
-        recevoirTemperature(villeChoisie);
-    });
-
-
-    function recevoirTemperature(ville) {   
-
-        const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=fb57a853ce1f2143b146f48c50216831&units=metric';
-
-        let requete =  new XMLHttpRequest();
-        requete.open('GET', url);
-        requete.responseType = 'json';
-        requete.send();
-
-        requete.onload = function() {
-            if (requete.readyState === XMLHttpRequest.DONE) {
-                if (requete.status === 200) {
-                    let reponse = requete.response;
-                    let temperature = reponse.main.temp;
-                    let ville = reponse.name;
-                    document.querySelector("#ville").textContent = ville;
-                    document.querySelector("#temperature_label").textContent = temperature;
-                    console.log("La température à " + ville + " est " + temperature + " degrés Celcius");
-                }
-                else {
-                    alert('Un problème est survenu, veillez revenir plutard.');
-                }
-            }
+requete.onload = function() {
+    if(requete.readyState === XMLHttpRequest.DONE) {    // Si la requête est terminée: si l'API ne nous envoie plus d'informations
+        if (requete.status === 200) {
+            let reponse = requete.response;
+            console.log(reponse);
+        }
+        else {
+            alert("Un problème est survenu, merci de revenir plutard.");
         }
     }
-
-
-
-
-   
 }
+
+
 
 
 
