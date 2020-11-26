9173456deceee11fe607b445b938aaea269c50e2
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-26 21:32:58 +0100

    Ajouter et supprimer un élément

--- exemple.js
+++ exemple.js
@@ -1,26 +1,19 @@
-/* Sélectionner et modifier un élément */
+/* Ajouter et supprmier un élément */
 
 // JavaScript
-document.querySelector('h1').textContent = "Bonjour avec JavaScript";
-// document.querySelectorAll('p').textContent = "Ceci est mon parapgraphe."; // Sélectionner tous les p. On ne peut en revanche pas modifier tous les p d'un coup,
-// Contrairement à ce que permet jQuery.  
-document.querySelector('.maClass').innerHTML = "<p><b>HTML modifié</b></p>"; // Modification du HTML
-document.querySelector('span').style.color = 'orange';      // Modifier le style
-// document.querySelector('#monId').className = 'important';   // Ajout d'une classe
-// document.querySelector('#monId').className = ''; // On ne peut pas supprimer une seule classe, on les supprime toutes
-// document.querySelectorAll('p').style.color = 'purple'; / On ne peut pas modifier le style de TOUS les éléments sélectionnés par 'All'.
+document.querySelector('h1').prepend('Oh : ');  // Ajouter un élément avant le contenu. 
+document.querySelector('h1').append(' !');      // Ajouter un élément après le contenu.
+document.querySelectorAll('p');
+
+
+
 
 // jQuery                                                                             
-$('h1').text('Bonjour avec jQuery'); // On donne le sélecteur CSS
-$('p').html('<p><b>HTML modifié avec jQuery</b></p>');  // On sélectionne ici tous les éléments p. On peut modifier uniquement leur texte si on veut, ou sinon tout leur html.
-$('span').css('color', 'green');     // On garde uniquement le 'color', si on veut juste appeler la propriété par ex. pour la ranger dans une variable.
-$('#monId').addClass('important');   // Ajout d'une classe avec jQuery.
-$('#monId').removeClass('important'); // On retire une seule classe à la fois.
-$('p').css('color','purple');         // On peut par contre modifier le style de tous les éléments 'p' avec jQuery.
-
-
-
-
+$('h1').prepend('Oh : ');  // Ajouter un élément avant le contenu. 
+$('h1').append(' !');      // Ajouter un élément après le contenu. 
+$('h1').before('<div>Hello !</div>'); // Ajouter un élément html avant le h1.
+$('h1').after('<div>Hello !<div/>');  // Ajouter un élément html après le h1.
+$('p').remove();           // Supprmier un élément.
 
 
 
