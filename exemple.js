e968c25b03a6425b3cd010c64158987ad3e21255
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-26 20:57:54 +0100

    Sélectionner, modifier le contenu et le style d'un élément

--- exemple.js
+++ exemple.js
@@ -1,67 +1,22 @@
+/* Sélectionner et modifier un élément */
 
+// JavaScript
+document.querySelector('h1').textContent = "Bonjour avec JavaScript";
+// document.querySelectorAll('p').textContent = "Ceci est mon parapgraphe."; // Sélectionner tous les p. On ne peut en revanche pas modifier tous les p d'un coup,
+// Contrairement à ce que permet jQuery.  
+document.querySelector('.maClass').innerHTML = "<p><b>HTML modifié</b></p>"; // Modification du HTML
+document.querySelector('span').style.color = 'orange';      // Modifier le style
+// document.querySelector('#monId').className = 'important';   // Ajout d'une classe
+// document.querySelector('#monId').className = ''; // On ne peut pas supprimer une seule classe, on les supprime toutes
+// document.querySelectorAll('p').style.color = 'purple'; / On ne peut pas modifier le style de TOUS les éléments sélectionnés par 'All'.
 
-
-let boutton = document.querySelector('#mode');
-let textBtn = document.querySelector('span');
-
-if(localStorage.getItem('theme')) {
-    if(localStorage.getItem('theme') === 'sombre') {
-        modeSombre();
-    }
-}
-
-
-boutton.addEventListener('click', () => {
-
-    if(document.body.classList.contains('dark')) {
-        document.body.className = '';
-        textBtn.textContent = "Thème sombre";
-        localStorage.setItem('theme', 'clair');
-    }
-    else {
-        modeSombre();
-    }
-});
-
-function modeSombre() {
-    document.body.className = 'dark';
-    textBtn.textContent = "Thème clair";
-    localStorage.setItem('theme', 'sombre');
-}
-
-
-
-
-// if(localStorage.getItem('prenom')) {    // Si l'élément existe déjà
-//     document.body.append('Bonjour ' + localStorage.getItem('prenom')); // L'item existe déja donc on y fait simplement appel par localStorage.getItem[clé] et on l'attache a notre page html via body.append()
-// }
-// else {                              // Sinon
-//     let prenom = prompt('Quel est votre prénom?');
-//     localStorage.setItem('prenom', prenom);        // Si on avait fait sessionStorage, on perdrait la donnée à la fermeture de la fenêtre
-//     document.body.append('Bonjour ' + prenom);
-// }
-
-// localStorage.clear(); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// jQuery                                                                             
+$('h1').text('Bonjour avec jQuery'); // On donne le sélecteur CSS
+$('p').html('<p><b>HTML modifié avec jQuery</b></p>');  // On sélectionne ici tous les éléments p. On peut modifier uniquement leur texte si on veut, ou sinon tout leur html.
+$('span').css('color', 'green');     // On garde uniquement le 'color', si on veut juste appeler la propriété par ex. pour la ranger dans une variable.
+$('#monId').addClass('important');   // Ajout d'une classe avec jQuery.
+$('#monId').removeClass('important'); // On retire une seule classe à la fois.
+$('p').css('color','purple');         // On peut par contre modifier le style de tous les éléments 'p' avec jQuery.
 
 
 
