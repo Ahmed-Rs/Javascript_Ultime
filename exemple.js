3a230c7ade0988e0c629e0e9fe4145f333ad5c1d
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-27 17:19:22 +0100

    Faire une requête AJAX

--- exemple.js
+++ exemple.js
@@ -1,40 +1,95 @@
-/* Ajouter des évènements */
+/* Faire une requête AJAX avec jQuery et JavaScript */
 
 
+// Avec JavaScript
 
-// JavaScript
-// document.querySelector('h1').addEventListener('click', () => {
-//    document.querySelector('h1').style.color = 'orange';
-// });
+// let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+// requete.open('GET', url); // Nous récupérons juste des données
+// requete.responseType = 'json'; // Nous attendons du JSON
+// requete.send(); // Nous envoyons notre requête
 
-
-// Executer une fct. dès qu'on click sur un des 'p'.
-// for(let i = 0; i < 2; i++) {
-//     document.querySelectorAll('p')[i].addEventListener('click', () => {
-//         document.querySelector('h1').style.color = 'green';
-//     });  
+// // Dès qu'on reçoit une réponse, cette fonction est executée
+// requete.onload = function() {
+//    if (requete.readyState === XMLHttpRequest.DONE) {
+//       if (requete.status === 200) {
+//         let reponse = requete.response;
+//         // console.log(reponse);
+//         let temperature = reponse.main.temp;
+//         let ville       = reponse.name;
+//         // console.log(temperature);
+//         document.querySelector('#temperature_label').textContent = temperature;
+//         document.querySelector('#ville').textContent = ville;
+//       }
+//       else {
+//         alert('Un problème est intervenu, merci de revenir plus tard.');
+//       }
+//     }
 // }
 
 
+// Avec jQuery
 
-// jQuery                                                                             
-// $('h1').click(() => {               
-//     $('h1').css('color', 'blue');
-// });
 
-$('p').click(() => {                // Déclencher un évènement lorsqu'on click sur p et qu'il y'en a plusieurs, est plus facile à faire avec jQuery.
-    $('h1').css('color', 'green');
+//GET
+const url = 'https://blockchain.info/ticker';
+
+$.ajax({
+    type: "GET",
+    url: url,
+    dataType: "json",
+    success: (data) => {             // Récupère la response de notre API, stocker tout le json que notre API va nous envoyer
+        // console.log(data);
+        $('h1').text(data.EUR.last + "€");
+    },
+    error: () => {
+        alert('Merci de revenir plus tard');
+    }
 });
 
 
-// On peut réutiliser les évènement JavaScript qu'on a l'habitude d'utiliser, en faisant on().
-$('h1').on('mouseover', () => {
-    $('#monId').addClass('important');
+
+//POST
+
+const url_bis = 'https://lesoublisdelinfo.com/api.php';
+
+$.ajax({
+    url: url_bis,
+    type: "POST",
+    data: "prenom=John",             // On utilise data lorsqu'on envoie (POST)
+    dataType: "json",
+    success: (data) => {
+        $('#monId').text(data.resultat);
+    },
+    error: () => {
+        alert('Merci de revenir plus tard.');
+    }
 });
 
-$('h1').on('mouseout', () => {
-    $('#monId').removeClass('important');
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
