b2d6be0caf12edf1c3a90d63d1cfc188ab252b45
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-27 12:22:23 +0100

    Ajouter des évènements

--- exemple.js
+++ exemple.js
@@ -1,20 +1,40 @@
-/* Modifier les attributs d'un élément */
+/* Ajouter des évènements */
+
+
 
 // JavaScript
-document.querySelector('h1').getAttribute('class'); // Sélectionne un attribut
-// document.querySelector('h1').setAttribute('style', 'text-transform: lowercase');
-document.querySelectorAll('p');
+// document.querySelector('h1').addEventListener('click', () => {
+//    document.querySelector('h1').style.color = 'orange';
+// });
+
+
+// Executer une fct. dès qu'on click sur un des 'p'.
+// for(let i = 0; i < 2; i++) {
+//     document.querySelectorAll('p')[i].addEventListener('click', () => {
+//         document.querySelector('h1').style.color = 'green';
+//     });  
+// }
 
 
 
 // jQuery                                                                             
-$('h1').attr('class'); // Sélectionne un attribut, retourne sa valeur.
-$('h1').attr('style', 'text-transform: lowercase'); // Quand on donne les deux paramètres, il ajoute ou modifie l'attribut.
-$('h1').after('<a href="https://wikipedia.com">Wikipedia</a>');
-$('a').attr('href', 'https://believemy.com');
-$('p');
+// $('h1').click(() => {               
+//     $('h1').css('color', 'blue');
+// });
+
+$('p').click(() => {                // Déclencher un évènement lorsqu'on click sur p et qu'il y'en a plusieurs, est plus facile à faire avec jQuery.
+    $('h1').css('color', 'green');
+});
 
 
+// On peut réutiliser les évènement JavaScript qu'on a l'habitude d'utiliser, en faisant on().
+$('h1').on('mouseover', () => {
+    $('#monId').addClass('important');
+});
+
+$('h1').on('mouseout', () => {
+    $('#monId').removeClass('important');
+});
 
 
 
